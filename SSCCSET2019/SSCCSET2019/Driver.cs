5a11e47326f76afc127af17ca5c7adc324a31403
Prokopets Aleksandr <prokopets.alexand@gmail.com>
2019-11-07 21:38:25 +0200

    Driver singeton added

---
+++ SSCCSET2019/SSCCSET2019/Driver.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SSCCSET2019
+{
+    public class Driver
+    {
+        private static Driver instance;
+        public IWebDriver driver = new ChromeDriver();
+
+        private Driver()
+        {
+        }
+
+        public static Driver getInstance()
+        {
+            if (instance == null)
+                instance = new Driver();
+
+            return instance;
+        }
+    }
+}
