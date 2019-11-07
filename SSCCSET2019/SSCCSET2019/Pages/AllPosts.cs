a482799c9e089528f6fd80019a6d51487eadb9c0
Prokopets Aleksandr <prokopets.alexand@gmail.com>
2019-11-07 23:43:08 +0200

    AllPostsPage start

---
+++ SSCCSET2019/SSCCSET2019/Pages/AllPosts.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SSCCSET2019.Pages
+{
+    class AllPosts
+    {
+        IWebDriver driver = Driver.GetInstance().driver;
+
+        IWebElement allLabel;
+        IWebElement publishedLabel;
+        IWebElement allPostsCount;
+        IWebElement publishedPostsCount;
+        IWebElement bulkActionSelectorTop;
+        IWebElement applyBtn;
+        IWebElement filterByDateSelector;
+        IWebElement categorySelector;
+        IWebElement filterBtn;
+        IWebElement displayingSumLabel;
+
+
+
+        public AllPosts()
+        {
+            allLabel = driver.FindElement(By.XPath(@"//li[@class='all']/a"));
+            allPostsCount = driver.FindElement(By.XPath(@"//li[@class='all']/a/span"));
+            publishedLabel = driver.FindElement(By.XPath("//li[@class='published']/a"));
+            publishedPostsCount = driver.FindElement(By.XPath(@"//li[@class='published']/span"));
+            bulkActionSelectorTop = driver.FindElement(By.Id("bulk-action-selector-top"));
+            applyBtn = driver.FindElement(By.Id("doaction"));
+            filterByDateSelector = driver.FindElement(By.Id("filter-by-date"));
+            categorySelector = driver.FindElement(By.Id("cat"));
+            filterBtn = driver.FindElement(By.Id("post-query-submit"));
+            displayingSumLabel = driver.FindElement(By.XPath("//span[@class='displaying-num']"));
+            driver.FindElement(By.Id(""));
+        }
+
+        public string GetAllLabel()
+        {
+            return allLabel.Text;
+        }
+
+        public string GetAllPostsCount()
+        {
+            return allPostsCount.Text;
+        }
+
+
+
+    }
+}
