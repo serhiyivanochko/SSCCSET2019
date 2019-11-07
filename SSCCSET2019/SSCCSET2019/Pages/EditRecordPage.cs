f2ec93f0ca466a4320b4879c4098a21dd4c3e090
Prokopets Aleksandr <prokopets.alexand@gmail.com>
2019-11-07 20:31:23 +0200

    Create EditRecordPage

---
+++ SSCCSET2019/SSCCSET2019/Pages/EditRecordPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCCSET2019.Pages
+{
+    class EditRecordPage
+    {
+        int a = 0;
+    }
+}
