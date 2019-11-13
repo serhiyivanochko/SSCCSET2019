ac00b45b5a520f2ab6c6a53edf08a9b0824f9102
Natalia Khomyn <natashkkaaa@gmail.com>
2019-11-13 18:43:24 +0200

    New

---
+++ SSCCSET2019/SSCCSET2019/Help_and_ScreenParameters/Contextual_help.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SSCCSET2019.Help_and_ScreenParameters
+{
+    public class Contextual_help
+    { IWebDriver driver;
+
+        //contextual-help-sidebar
+        public IWebElement MoreInfoLabel
+        { get { return driver.FindElement(By.XPath("//*[@id='contextual-help-columns']/div[2]/p[1]/strong")); } }
+        public IWebElement CommentsDocs
+        { get { return driver.FindElement(By.XPath("//*[contains (@href,'https://codex.wordpress.org/Administration_Screens#Comments')]")); } }
+        public IWebElement CommentsSpamDocs
+        { get { return driver.FindElement(By.XPath("//*contains (@href,'https://codex.wordpress.org/Comment_Spam')]")); } }
+        public IWebElement HotKeyDocs
+        { get { return driver.FindElement(By.XPath("//* [contains (@href,'https://codex.wordpress.org/Keyboard_Shortcuts')]")); } }
+        public IWebElement SupportForum
+        { get { return driver.FindElement(By.XPath("//* [contains (@href,'https://uk.wordpress.org/support/')]")); } }
+
+        //contextual-help-back
+
+        public IWebElement ContextualHelpBack
+        { get { return driver.FindElement(By.Id("contextual-help-back")); } }
+
+        //contextual-help-tabs
+        public IWebElement Overview
+        { get { return driver.FindElement(By.Id("tab-link-overview")); } }
+        public IWebElement ModeratingComments
+        { get { return driver.FindElement(By.Id("tab - link - moderating - comments")); } }
+
+
+    }
+}
---
+++ SSCCSET2019/SSCCSET2019/Help_and_ScreenParameters/ScreenOptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SSCCSET2019.Help_and_ScreenParameters
+{
+      public class ScreenOptions
+    {
+        IWebDriver driver;
+        //Column Constructor Buttoms
+        public IWebElement ColumnsLabel
+        { get { return driver.FindElement(By.XPath("//*[@id='adv-settings']/fieldset[1]/legend")); } }
+        public IWebElement AuthorLabel
+        { get { return driver.FindElement(By.Id("author-hide")); } }
+        public IWebElement ResponseLabel
+        { get { return driver.FindElement(By.Id("response - hide")); } }
+        public IWebElement SendedLabel
+        { get { return driver.FindElement(By.Id("date - hide")); } }
+
+        //Page Numbering
+        public IWebElement RecordNumberLabel
+        { get { return driver.FindElement(By.XPath("//*[@id='adv-settings']/fieldset[2]/legend")); } }
+        public IWebElement NumberPageLabel
+        { get { return driver.FindElement(By.XPath("//label[contains(@for,'edit_comments_per_page')]")); } }
+        public IWebElement NumberPageEdit
+        { get { return driver.FindElement(By.Id("edit_comments_per_page")); } }
+
+        public IWebElement SubmitButton
+        { get { return driver.FindElement(By.Id("screen - options - apply")); } }
+
+    }
+}
