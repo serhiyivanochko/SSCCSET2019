deeca52d6c8f6fbdcbaacf9e71e61c2b7445fbe6
VovaKryvonos <47331984+VovaKryvonos@users.noreply.github.com>
2019-11-13 18:39:50 +0200

    Update Сategories.cs

--- SSCCSET2019/SSCCSET2019/Pages/Notes/Сategories.cs
+++ SSCCSET2019/SSCCSET2019/Pages/Notes/Сategories.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium;
 namespace SSCCSET2019.Pages.Notes
 {
     class Сategories
@@ -6,58 +7,102 @@ namespace SSCCSET2019.Pages.Notes
         IWebDriver driver;
 
 
-        IWebElement name = driver.FindElement(By.Id("tag-name"));
-        IWebElement nameDescription = driver.FindElement(By.Id("//*[@id=\"addtag\"]/div[1]/p"));
-        IWebElement partOfURL = driver.FindElement(By.Id("tag-slug"));
-        IWebElement partOfURLDescription = driver.FindElement(By.Id("//*[@id=\"addtag\"]/div[2]/p"));
-        IWebElement fatherCategory = driver.FindElement(By.Id("postform"));
-        IWebElement fatherCategoryDescription = driver.FindElement(By.Id("//*[@id=\"addtag\"]/div[3]/p"));
-        IWebElement description = driver.FindElement(By.Id("tag-description"));
-        IWebElement descriptionForDescription = driver.FindElement(By.XPath("//*[@id=\"addtag\"]/div[4]/p"));
-        IWebElement submit = driver.FindElement(By.Id("submit"));
-        IWebElement WordPressURL = driver.FindElement(By.XPath("//*[@id=/\"footer-thankyou\"]/a"));
+        IWebElement name;
+        IWebElement nameDescription;
+        IWebElement partOfURL;
+        IWebElement partOfURLDescription;
+        IWebElement fatherCategory;
+        IWebElement fatherCategoryDescription;
+        IWebElement description;
+        IWebElement descriptionForDescription;
+        IWebElement submit;
+        IWebElement WordPressURL;
 
 
-        IWebElement screenSettings = driver.FindElement(By.Id("show-settings-link"));
-        IWebElement column = driver.FindElement(By.XPath("//*[@id=\"adv - settings\"]/fieldset[1]/legend"));
-        IWebElement includeDecription = driver.FindElement(By.Id("description-hide"));
-        IWebElement includePartURL = driver.FindElement(By.Id("slug-hide"));
-        IWebElement includeCount = driver.FindElement(By.Id("posts-hide"));
-        IWebElement maxNumber = driver.FindElement(By.Id("edit_category_per_page"));
-        IWebElement btnSubmitSettings = driver.FindElement(By.Id("screen-options-apply"));
+        IWebElement screenSettings;
+        IWebElement column;
+        IWebElement includeDecription;
+        IWebElement includePartURL;
+        IWebElement includeCount;
+        IWebElement maxNumber;
+        IWebElement btnSubmitSettings;
 
 
-        IWebElement help = driver.FindElement(By.Id("contextual-help-link"));
-        IWebElement overviewHelp = driver.FindElement(By.XPath("//*[@id=\"tab - link - overview\"]/a"));
-        IWebElement overviewHelp = driver.FindElement(By.Id("tab-panel-overview"));
-        IWebElement addCategoriesHelp = driver.FindElement(By.XPath("//*[@id=\"tab - link - adding - terms\"]/a"));
-        IWebElement addCategoriesTextHelp = driver.FindElement(By.Id("tab-panel-adding-terms"));
-        IWebElement documentationHelp = driver.FindElement(By.XPath("//*[@id=\"contextual - help - columns\"]/div[2]/p[2]/a"));
-        IWebElement forumHelp = driver.FindElement(By.XPath("//*[@id=\"contextual - help - columns\"]/div[2]/p[3]/a"));
-        IWebElement rightPanelHelp = driver.FindElement(By.XPath("//*[@id=\"contextual - help - columns\"]/div[2]"));
+        IWebElement help;
+        IWebElement overviewHelp;
+        IWebElement addCategoriesHelp;
+        IWebElement addCategoriesTextHelp;
+        IWebElement documentationHelp;
+        IWebElement forumHelp;
+        IWebElement rightPanelHelp;
 
 
-        IWebElement editCategoryToFind = driver.FindElement(By.Id("tag-search-input"));
-        IWebElement btnFindCategory = driver.FindElement(By.Id("search-submit"));
+        IWebElement editCategoryToFind;
+        IWebElement btnFindCategory;
 
 
-        IWebElement checkBoxSelectAll_upper = driver.FindElement(By.Id("cb-select-all-1"));
-        IWebElement checkBoxSelectAll_lower = driver.FindElement(By.Id("cb-select-all-2"));
-        IWebElement sortByName_upper = driver.FindElement(By.XPath("//*[@id=\"name\"]/a/span[2]"));
-        IWebElement sortByName_lower = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/table/tfoot/tr/th[1]/a/span[2]"));
-        IWebElement sortByURL_upper = driver.FindElement(By.XPath("//*[@id=\"slug\"]/a/span[2]"));
-        IWebElement sortByURL_lower = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/table/tfoot/tr/th[3]/a/span[2]"));
-        IWebElement actionSelector_upper = driver.FindElement(By.Id("bulk-action-selector-top"));
-        IWebElement apply_upper = driver.FindElement(By.Id("doaction"));
-        IWebElement actionSelector_lower = driver.FindElement(By.Id("bulk-action-selector-bottom"));
-        IWebElement apply_lower = driver.FindElement(By.Id("doaction2"));
-        IWebElement numberOfElements_upper = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/div[1]/div[2]/span[1]"));
-        IWebElement numberOfElements_lower = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/div[2]/div[2]/span[1]"));
+        IWebElement checkBoxSelectAll_upper;
+        IWebElement checkBoxSelectAll_lower;
+        IWebElement sortByName_upper;
+        IWebElement sortByName_lower;
+        IWebElement sortByURL_upper;
+        IWebElement sortByURL_lower;
+        IWebElement actionSelector_upper;
+        IWebElement apply_upper;
+        IWebElement actionSelector_lower;
+        IWebElement apply_lower;
+        IWebElement numberOfElements_upper;
+        IWebElement numberOfElements_lower;
 
 
-        IWebElement transformCategoryInMarkURL = driver.FindElement(By.XPath("//*[@id=\"col - right\"]/div/div/p[2]/a"));
-        IWebElement descriptionBox = driver.FindElement(By.XPath("//*[@id=\"col-right\"]/div/div"));
-        IWebElement version = driver.FindElement(By.Id("footer-upgrade"));
+        IWebElement transformCategoryInMarkURL;
+        IWebElement descriptionBox;
+        IWebElement version;
 
+        public Сategories(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.name = driver.FindElement(By.Id("tag-name"));
+            this.nameDescription = driver.FindElement(By.Id("//*[@id=\"addtag\"]/div[1]/p"));
+            this.partOfURL = driver.FindElement(By.Id("tag-slug"));
+            this.partOfURLDescription = driver.FindElement(By.Id("//*[@id=\"addtag\"]/div[2]/p"));
+            this.fatherCategory =driver.FindElement(By.Id("parent"));
+            this.fatherCategoryDescription = driver.FindElement(By.Id("//*[@id=\"addtag\"]/div[3]/p"));
+            this.description = driver.FindElement(By.Id("tag-description"));
+            this.descriptionForDescription =  driver.FindElement(By.XPath("//*[@id=\"addtag\"]/div[4]/p"));
+            this.submit = driver.FindElement(By.Id("submit"));
+            WordPressURL = driver.FindElement(By.XPath("//*[@id=/\"footer-thankyou\"]/a"));
+            this.screenSettings = driver.FindElement(By.Id("show-settings-link"));
+            this.column = driver.FindElement(By.XPath("//*[@id=\"adv - settings\"]/fieldset[1]/legend"));
+            this.includeDecription = driver.FindElement(By.Id("description-hide"));
+            this.includePartURL = driver.FindElement(By.Id("slug-hide"));
+            this.includeCount = driver.FindElement(By.Id("posts-hide"));
+            this.maxNumber = driver.FindElement(By.Id("edit_category_per_page"));
+            this.btnSubmitSettings = driver.FindElement(By.Id("screen-options-apply"));
+            this.help = driver.FindElement(By.Id("contextual-help-link"));
+            this.overviewHelp = driver.FindElement(By.XPath("//*[@id=\"tab - link - overview\"]/a"));
+            this.addCategoriesHelp = driver.FindElement(By.XPath("//*[@id=\"tab - link - adding - terms\"]/a"));
+            this.addCategoriesTextHelp = driver.FindElement(By.Id("tab-panel-adding-terms"));
+            this.documentationHelp = driver.FindElement(By.XPath("//*[@id=\"contextual - help - columns\"]/div[2]/p[2]/a"));
+            this.forumHelp = driver.FindElement(By.XPath("//*[@id=\"contextual - help - columns\"]/div[2]/p[3]/a"));
+            this.rightPanelHelp = driver.FindElement(By.XPath("//*[@id=\"contextual - help - columns\"]/div[2]"));
+            this.editCategoryToFind = driver.FindElement(By.Id("tag-search-input"));
+            this.btnFindCategory = driver.FindElement(By.Id("search-submit"));
+            this.checkBoxSelectAll_upper = driver.FindElement(By.Id("cb-select-all-1"));
+            this.checkBoxSelectAll_lower = driver.FindElement(By.Id("cb-select-all-2"));
+            this.sortByName_upper = driver.FindElement(By.XPath("//*[@id=\"name\"]/a/span[2]"));
+            this.sortByName_lower = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/table/tfoot/tr/th[1]/a/span[2]"));
+            this.sortByURL_upper = driver.FindElement(By.XPath("//*[@id=\"slug\"]/a/span[2]"));
+            this.sortByURL_lower = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/table/tfoot/tr/th[3]/a/span[2]"));
+            this.actionSelector_upper = driver.FindElement(By.Id("bulk-action-selector-top"));
+            this.apply_upper = driver.FindElement(By.Id("doaction"));
+            this.actionSelector_lower = driver.FindElement(By.Id("bulk-action-selector-bottom"));
+            this.apply_lower = driver.FindElement(By.Id("doaction2"));
+            this.numberOfElements_upper = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/div[1]/div[2]/span[1]"));
+            this.numberOfElements_lower = driver.FindElement(By.XPath("//*[@id=\"posts - filter\"]/div[2]/div[2]/span[1]"));
+            this.transformCategoryInMarkURL = driver.FindElement(By.XPath("//*[@id=\"col - right\"]/div/div/p[2]/a"));
+            this.descriptionBox = driver.FindElement(By.XPath("//*[@id=\"col-right\"]/div/div"));
+            this.version = driver.FindElement(By.Id("footer-upgrade"));
+        }
     }
 }
