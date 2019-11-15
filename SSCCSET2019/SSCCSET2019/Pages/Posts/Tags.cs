7682827132c9fc9fa83fb358f28d5816f1447c1e
NazariyChetvertukha <carrrson2207@gmail.com>
2019-11-15 15:01:33 +0200

    Update

--- SSCCSET2019/SSCCSET2019/Pages/Posts/Tags.cs
+++ SSCCSET2019/SSCCSET2019/Pages/Posts/Tags.cs
@@ -20,11 +20,9 @@ namespace SSCCSET2019.Pages.Tags
         protected IWebElement searchBoxTags;
         protected IWebElement searchTagsButton;
         protected IWebElement textUnderBottonBulkAction;
-
         protected IWebElement convertHyperlink;
         protected IWebElement displayingNumberOfTags;
         protected IWebElement nameOfVersion;
-
         protected IWebElement screenOptionsDropDown;
         protected IWebElement helpDropDown;
         protected IWebElement nameEdit;
@@ -34,38 +32,27 @@ namespace SSCCSET2019.Pages.Tags
         protected IWebElement tagsLabel;
         protected IWebElement addNewTagLabel;
         protected IWebElement nameLabel;
-
         protected List<IWebElement> listOfAddedTags;
         protected IWebElement tagsTable;
 
         public Tags(IWebDriver driver)
         {
             this.driver = driver;
-
             actionSelector = driver.FindElement(By.Id("bulk-action-selector-top"));
             doActionButton = driver.FindElement(By.Id("doaction"));
-
             checkBoxAll = driver.FindElement(By.Id("cb-select-all-1"));
             displayingNumberOfTags = driver.FindElement(By.ClassName("displaying-num"));
             nameOfVersion = driver.FindElement(By.Id("footer-upgrade"));
-
-
             tagsTable = driver.FindElement(By.Id("the-list"));
-
             listOfAddedTags = tagsTable.FindElements(By.TagName("tr")).ToList();
-
-
             countSortingIndicator = driver.FindElement(By.TagName("thead")).FindElement(By.Id("posts"));
             slugSortingIndicator = driver.FindElement(By.TagName("thead")).FindElement(By.Id("slug"));
             descriptionSortingIndicator = driver.FindElement(By.TagName("thead")).FindElement(By.Id("description"));
             nameSortingIndicator = driver.FindElement(By.TagName("thead")).FindElement(By.Id("name"));
-
             searchBoxTags = driver.FindElement(By.Id("tag-search-input"));
             searchTagsButton = driver.FindElement(By.Id("search-submit"));
-
-            textUnderBottonBulkAction = driver.FindElement(By.XPath(@"//*[@id='col-right']/div/div/p"));          
+            textUnderBottonBulkAction = driver.FindElement(By.XPath(@"//*[@id='col-right']/div/div/p"));
             convertHyperlink = driver.FindElement(By.CssSelector("div.form-wrap:nth-child(2) > p:nth-child(1) > a:nth-child(1)"));
-
             screenOptionsDropDown = driver.FindElement(By.Id("show-settings-link"));
             helpDropDown = driver.FindElement(By.Id("contextual-help-link-wrap"));
             nameEdit = driver.FindElement(By.Id("tag-name"));
@@ -75,40 +62,30 @@ namespace SSCCSET2019.Pages.Tags
             tagsLabel = driver.FindElement(By.ClassName("wp-heading-inline"));
             addNewTagLabel = driver.FindElement(By.XPath("//*[@id='col-left']/div/div/h2"));
             nameLabel = driver.FindElement(By.XPath("//*[@id='addtag']/div[1]/label"));
-
-
-
-
         }
 
         public void ClearSearchTagsBox()
         {
             searchBoxTags.Clear();
         }
-
         public Tags ClickSearchTagsButton()
         {
             searchTagsButton.Click();
             return this;
         }
-
         public void InputSearchTagsBox(string searchedTag)
         {
             searchBoxTags.SendKeys(searchedTag);
         }
-
         public void ClickSearchTagsBox()
         {
             searchBoxTags.Click();
         }
-
         public Tags ClickDoActionButton()
         {
             doActionButton.Click();
             return this;
         }
-
-
         public Tags ClickNameSortingIndicator()
         {
             nameSortingIndicator.Click();
@@ -129,49 +106,39 @@ namespace SSCCSET2019.Pages.Tags
             countSortingIndicator.Click();
             return this;
         }
-
         public void Click‘ctionSelector()
         {
             actionSelector.Click();
         }
-
         public string ClickHyperLink()
         {
             convertHyperlink.Click();
             return "http://localhost/wordpress/wp-admin/import.php";
         }
-
         public string ClickGetVersionLink()
         {
             nameOfVersion.Click();
             return "http://localhost/wordpress/wp-admin/update-core.php";
         }
-
         public string GetTagsLabel()
         {
             return tagsLabel.Text;
         }
-
         public string GetAddNewTagLabel()
         {
             return addNewTagLabel.Text;
         }
-
         public string GetNameLabel()
         {
             return nameLabel.Text;
         }
-
-
         public void ClickAddNewTagButton()
         {
             addNewTagButton.Click();
         }
-
         public void ClickCheckAll()
         {
             checkBoxAll.Click();
         }
     }
-
 }
