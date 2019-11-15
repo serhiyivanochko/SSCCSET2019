839700dda95b334e5b5ab344de05613105dc2460
MrStriker4013 <57452731+MrStriker4013@users.noreply.github.com>
2019-11-15 16:56:18 +0200

    Add files via upload

---
+++ SSCCSET2019/SSCCSET2019/Logic/ComentActionsLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SSCCSET2019.SSCCSET2019.Pages.CommentActionsPage;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace CommentActionsLogicPage
+{
+    public class CommentActionsLogicPage // потрібно поміняти назви сторінок на назви page з коментарями
+    {
+        protected IWebDriver driver;
+        public CommentActionsPage InputSeachTop(string text)
+        {
+            CommentActionsPage items = new CommentActionsPage();
+            items.ClickSearchTop();
+            items.ClearSearchTop();
+            items.SetSearchTop(text);
+            items.ClickButtonSearch();
+            return new CommentActionsPage(); // need to change
+        }
+        public CommentActionsPage SubmitCommentType(string commentType)
+        {
+            CommentActionsPage items = new CommentActionsPage();
+            items.SetCommentTypeSelect(commentType);
+            items.ClickCommentTypeSubmitButton();
+            return new CommentActionsPage(); // need to change
+        }
+        public CommentActionsPage SubmitFilterType(string filterType)
+        {
+            CommentActionsPage items = new CommentActionsPage();
+            items.SetFilterTypeSelect(filterType);
+            items.ClickCommentTypeSubmitButton();
+            return new CommentActionsPage(); // need to change
+        }
+        public CommentActionsPage CountOfElements(int a) // need to be done
+        {
+
+        }
+    }
+
+}
--- SSCCSET2019/SSCCSET2019/Pages/CommentActionsPage.cs
+++ SSCCSET2019/SSCCSET2019/Pages/CommentActionsPage.cs
@@ -6,41 +6,40 @@ using OpenQA.Selenium.Interactions;
 
 namespace SSCCSET2019.SSCCSET2019.Pages.CommentActionsPage
 {
-    public class CommentActionsPage : ATopCommentAndResponse 
+    public class CommentActionsPage
     {
-        public const string VALUE_ATTRIBUTE = "value";
-        public SelectElement CommentTypeSelect;
-        public IWebElement CommentTypeSubmitButton;
-        public SelectElement FilterTypeSelect;
-        public IWebElement FilterTypeSubmitButton;
-        public IWebElement CountElementsLabel;
-        public IWebElement SearchTop;
-        public IWebElement ButtonSearch;
-        public CommentActionsPage(IWebDriver driver) : base(driver)
-        {          
-            CommentTypeSelect = new SelectElement(driver.FindElement(By.Id("bulk-action-selector-top")));
-            IWebElement CommentTypeSubmitButton = driver.FindElement(By.Id("doaction"));
-            FilterTypeSelect = new SelectElement(driver.FindElement(By.Id("filter-by-comment-type")));
-            IWebElement FilterTypeSubmitButton = driver.FindElement(By.Id("post-query-submit"));
-            IWebElement CountElementsLabel = driver.FindElement(By.XPath("//*[@id='comments - form']/div[1]/div[3]/span[1]"));
-            IWebElement SearchTop = driver.FindElement(By.Id("comment-search-input"));
-            IWebElement ButtonSearch = driver.FindElement(By.Id("search-submit"));
-        }
+        protected IWebDriver driver;
+        public SelectElement CommentTypeSelect
+        { get { return new SelectElement(driver.FindElement(By.Id("bulk-action-selector-top"))); } }
+        public IWebElement CommentTypeSubmitButton 
+        { get { return driver.FindElement(By.Id("doaction")); } }
+        public SelectElement FilterTypeSelect 
+        { get { return new SelectElement(driver.FindElement(By.Id("filter-by-comment-type"))); } }
+        public IWebElement FilterTypeSubmitButton 
+        { get { return driver.FindElement(By.Id("post-query-submit")); } }
+        public IWebElement CountElementsLabel
+        { get { return driver.FindElement(By.XPath("//*[@id='comments - form']/div[1]/div[3]/span[1]")); } }
+        public IWebElement SearchTop 
+        { get { return driver.FindElement(By.Id("comment-search-input")); } }
+        public IWebElement ButtonSearch
+        { get { return driver.FindElement(By.Id("search-submit"));  } }      
         public string GetCountElementsLabelText()
         {
             return CountElementsLabel.Text;
         }
-        public void ClickCommentTypeSubmitButton()
+        public CommentActionsPage ClickCommentTypeSubmitButton()
         {
             CommentTypeSubmitButton.Click();
+            return this;
         }
         public string GetCommentTypeSubmitButtonText()
         {
             return CommentTypeSubmitButton.Text;
         }
-        public void ClickFilterTypeSelectSubmitButtonTop2()
+        public CommentActionsPage ClickFilterTypeSelectSubmitButtonTop2()
         {
             FilterTypeSubmitButton.Click();
+            return this;
         }
         public string GetFilterTypeSubmitButtonText()
         {
@@ -50,21 +49,24 @@ namespace SSCCSET2019.SSCCSET2019.Pages.CommentActionsPage
         {
             return CommentTypeSelect.AllSelectedOptions[0].Text;
         }
-        public void SetCommentTypeSelect1(string commentType)
+        public CommentActionsPage SetCommentTypeSelect(string commentType)
         {
             CommentTypeSelect.SelectByText(commentType);
+            return this;
         }
         public string GetFilterTypeSelectText()
         {
             return FilterTypeSelect.AllSelectedOptions[0].Text;
         }
-        public void SetFilterTypeSelect(string filterType)
+        public CommentActionsPage SetFilterTypeSelect(string filterType)
         {
             FilterTypeSelect.SelectByText(filterType);
+            return this;
         }
-        public void ButtonSearchClick()
+        public CommentActionsPage ClickButtonSearch()
         {
             ButtonSearch.Click();
+            return this;
         }
         public string GetButtonSearchText()
         {
@@ -72,16 +74,25 @@ namespace SSCCSET2019.SSCCSET2019.Pages.CommentActionsPage
         }
         public string GetSearchTopInputText()
         {
-            return SearchTop.GetAttribute(VALUE_ATTRIBUTE);
+            return this.SearchTop.Text;
         }
-        public void SetSearchTop(string text)
+        public CommentActionsPage SetSearchTop(string text)
+        {          
+            this.SearchTop.SendKeys(text);
+            return this;
+        }
+        public CommentActionsPage ClearSearchTop()
+        {
+            SearchTop.Clear();
+            return this;
+        }
+        public CommentActionsPage ClickSearchTop()
         {
             SearchTop.Click();
-            SearchTop.Clear();
-        }
-        public void ClearSearchTop()
-        {
-            SearchTop.Clear();
+            return this;
         }
     }
+    
+
+
 }
