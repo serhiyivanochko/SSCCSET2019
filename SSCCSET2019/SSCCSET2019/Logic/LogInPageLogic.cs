a5501ecb02539d7be46cc6635465660a5b9d3075
vasul <wasilius@gmail.com>
2019-11-22 15:34:34 +0200

    add LoginPage and LoginPageLogic

---
+++ SSCCSET2019/SSCCSET2019/Logic/LogInPageLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SSCCSET2019.Pages;
+using SSCCSET2019.Pages.LogInPage;
+
+namespace SSCCSET2019.Logic
+{
+    class LoginPageLogic
+    {
+
+
+
+        public void LogInWordPress(string login, string password, IWebDriver driver)
+        {
+            LoginPage logIn = new LoginPage(driver);            
+            logIn.ClickOnLoginField();
+            logIn.ClearLoginField();
+            logIn.SetTextInLoginField(login);
+            logIn.ClickOnPasswordField();
+            logIn.ClearPasswordField();
+            logIn.SetTextInPasswordField(password);
+            logIn.ClickSubmitButton();
+            //return new MainPage();
+        }
+    }
+}
---
+++ SSCCSET2019/SSCCSET2019/Pages/LogInPage/LogInPage.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCCSET2019.Pages.LogInPage
+{
+    class LoginPage
+    {
+        private IWebElement login;
+        private IWebElement pswrd;
+        private IWebElement rmbrCheckBox;
+        private IWebElement lgBtn;
+        private IWebElement lostPswrd;
+        private IWebElement back;
+
+        public LoginPage(IWebDriver driver) 
+        {
+            login = driver.FindElement(By.Id("user_login"));
+            pswrd = driver.FindElement(By.Id("user_pass"));
+            rmbrCheckBox = driver.FindElement(By.Id("rememberme"));
+            lgBtn = driver.FindElement(By.Id("wp-submit"));
+            lostPswrd = driver.FindElement(By.XPath(@"//*[@id='nav']/at"));
+            back = driver.FindElement(By.Id(@"//*[@id='backtoblog']/a"));
+        }
+
+        public LoginPage ClickOnLoginField()
+        {
+            login.Click();
+            return this;
+        }
+        public LoginPage ClearLoginField()
+        {
+            login.Clear();
+            return this;
+        }
+        public LoginPage SetTextInLoginField(string userName)
+        {
+            login.SendKeys(userName);
+            return this;
+        }
+         public LoginPage ClickOnPasswordField()
+        {
+            pswrd.Click();
+            return this;
+        }
+        public LoginPage ClearPasswordField()
+        {
+            pswrd.Clear();
+            return this;
+        }
+        public LoginPage SetTextInPasswordField(string password)
+        {
+            pswrd.SendKeys(password);
+            return this;
+        }
+
+        public void ClickSubmitButton() 
+        {
+            lgBtn.Click();
+            //return new MainPage();
+        }
+
+        public LoginPage ClickOnCheckBox() 
+        {
+            rmbrCheckBox.Click();
+            return this;
+        }
+
+    }
+}
