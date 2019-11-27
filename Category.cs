1f75c1164a72a1959190eda8f74f6d71550970b7
VovaKryvonos <47331984+VovaKryvonos@users.noreply.github.com>
2019-11-27 19:01:05 +0200

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium;
+namespace SSCCSET2019.Pages.Notes
+{
+    class Category
+    {
+        private IWebDriver driver;
+
+        private IWebElement CheckBox;
+        private IWebElement Title;
+        private IWebElement Edit;
+        private IWebElement Property;
+        private IWebElement Delete;
+        private IWebElement Review;
+        private IWebElement PartOfLink;
+
+        public Category(IWebDriver driver,string id)
+        {
+            this.driver = driver;
+            CheckBox =driver.FindElement(By.Id("cb-select-"+id[id.Length-1]));
+            Title = driver.FindElement(By.XPath(String.Format("//*[@id=\"%s\"]/td[1]/strong/a", id)));
+            Edit = driver.FindElement(By.XPath(String.Format("//*[@id=\"%s\"]/td[1]/div[2]/span[1]/a", id)));
+            Delete = driver.FindElement(By.XPath(String.Format("//*[@id=\"%s\"]/td[1]/div[2]/span[3]/button", id)));
+            Review = driver.FindElement(By.XPath(String.Format("//*[@id=\"%s\"]/td[1]/div[2]/span[4]/a", id)));
+            Property = driver.FindElement(By.XPath(String.Format("//*[@id=\"%s\"]/td[1]/div[2]/span[2]/a", id)));
+            PartOfLink = driver.FindElement(By.XPath(String.Format("//*[@id=\"%s\"]/td[3]", id))); 
+        }
+    }
+}
