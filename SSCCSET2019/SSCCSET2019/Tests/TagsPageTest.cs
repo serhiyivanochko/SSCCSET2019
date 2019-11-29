cc86fdc3021a8716fe3e8a3796afbdd9e75872f9
NazariyChetvertukha <carrrson2207@gmail.com>
2019-11-29 13:58:21 +0200

    Using LoginLogic to login user

--- SSCCSET2019/SSCCSET2019/Tests/TagsPageTest.cs
+++ SSCCSET2019/SSCCSET2019/Tests/TagsPageTest.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using SSCCSET2019.Tools.Driver;
 using SSCCSET2019.Logic;
+using SSCCSET2019.Logic.LoginPageLogic;
 
 namespace SSCCSET2019.Tests
 {
@@ -11,25 +12,10 @@ namespace SSCCSET2019.Tests
         [SetUp]
         public void Setup()
         {
-            IWebDriver driver = Driver.GetDriver();
-            driver.Navigate().GoToUrl("http://localhost/wordpress/wp-admin/edit-tags.php?taxonomy=post_tag");
-            var login = driver.FindElement(By.Id("user_login"));
-            var password = driver.FindElement(By.Id("user_pass"));
-            login.SendKeys("Nazariy");
-            password.SendKeys("wordpresswordpess");
-            var submit = driver.FindElement(By.Id("wp-submit"));
-            submit.Click();
+            Driver.GetDriver().Navigate().GoToUrl("http://localhost/wordpress/wp-admin/edit-tags.php?taxonomy=post_tag");
+            new LoginPageLogic().LogInWordPress("Nazariy", "wordpresswordpess");
         }
-        [Test]
-        public void TestCase()
-        {
-            TagsPageLogic tagsPage = new TagsPageLogic();
-            string tagName = "Test Tag";
-            string tagSlug = "Test Slug";
-            tagsPage.AddNewTagsWithSlug(tagName, tagSlug);
-            bool temp = tagsPage.IfFoundedTags(tagName);
-            Assert.AreEqual(temp, true);
-        }
+     
         [Test]
         public void TestAddNewTagsWithSlugAndDescription()
         {
